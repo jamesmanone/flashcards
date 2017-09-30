40f73292719e0a6252c9390f430bf9c99b81f416
jamesmanone <boostedx@me.com>
2017-09-30 12:47:59 -0400

    fixed refactoring error in newcard

--- components/NewCard/index.js
+++ components/NewCard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Alert } from 'react-native';
 import { addCard } from '../../actions/cardActions';
 import CardForm from './CardForm';
 
